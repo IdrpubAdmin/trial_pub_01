4affd1ee644298d7777c23d340589cdc05899007
hyeona1004 <mlmhusdk14@hanmail.net>
2021-03-18 17:55:59 +0900

    lib에 압축된 버전의 파일 생성

--- Seum/js/lib/common.js
+++ Seum/js/lib/common.js
@@ -1,58 +1,10 @@
-'use strict';
-
-// btn_toggle 클릭 event
-var toggleBtn = document.querySelector('.btn_toogle');
-var menu = document.querySelector('#mo_gnb');
-
-toggleBtn.addEventListener('click', function () {
-    menu.classList.toggle('active');
-});
-
-// gnb mouse over N out event
-document.addEventListener('DOMContentLoaded', function () {
-    var toggleBtns = document.querySelectorAll('.gnb_1depth>li');
-    //console.log(toggleBtns)
-
-    toggleBtns.forEach(function (navItem, idx, list) {
-        //console.log(navItem)
-
-        var navItem1d = navItem.querySelector('.gnb_1depth>li>a');
-        var navItem2d = navItem.querySelector('.gnb_2depth');
-
-        // console.log(navItem2d)
-
-        // 1depth에 마우스 오버시 2depth 노출
-        navItem.addEventListener('mouseover', function () {
-            navItem2d.classList.add('active');
-        });
-        navItem.addEventListener('mouseout', function () {
-            navItem2d.classList.remove('active');
-        });
-
-        // 2depth에 마우스 오버시 1depth 배경색 변경
-        navItem2d.addEventListener('mouseover', function () {
-            navItem1d.classList.add('active_02');
-        });
-        navItem2d.addEventListener('mouseout', function () {
-            navItem1d.classList.remove('active_02');
-        });
-    });
-});
-if (window.NodeList && !NodeList.prototype.forEach) {
-    NodeList.prototype.forEach = Array.prototype.forEach;
-}
-// 브로드 캐스트
+'use strict';// btn_toggle 클릭 event
+var toggleBtn=document.querySelector('.btn_toogle'),menu=document.querySelector('#mo_gnb');toggleBtn.addEventListener('click',function(){menu.classList.toggle('active')}),document.addEventListener('DOMContentLoaded',function(){var a=document.querySelectorAll('.gnb_1depth>li');//console.log(toggleBtns)
+a.forEach(function(a){//console.log(navItem)
+var b=a.querySelector('.gnb_1depth>li>a'),c=a.querySelector('.gnb_2depth');// console.log(navItem2d)
+// 1depth에 마우스 오버시 2depth 노출
+// 2depth에 마우스 오버시 1depth 배경색 변경
+a.addEventListener('mouseover',function(){c.classList.add('active')}),a.addEventListener('mouseout',function(){c.classList.remove('active')}),c.addEventListener('mouseover',function(){b.classList.add('active_02')}),c.addEventListener('mouseout',function(){b.classList.remove('active_02')})})}),window.NodeList&&!NodeList.prototype.forEach&&(NodeList.prototype.forEach=Array.prototype.forEach);// 브로드 캐스트
 //const title = document.getElementById("cast_title").textContent;
-var title = document.querySelector('.container_box>.title').textContent;
-document.querySelector('#cast').innerHTML = title;
-
-// 활성화 페이지 서브타이틀 컬러 넣어주기
-(function () {
-    var loc = window.location.href;
-    var dep2Gnb = document.querySelectorAll('.gnb_2depth > li > a');
-    dep2Gnb.forEach(function (obj) {
-        if (loc.indexOf(obj.getAttribute("href")) != -1) {
-            obj.classList.add('on');
-        }
-    });
-})();
+var title=document.querySelector('.container_box>.title').textContent;// 활성화 페이지 서브타이틀 컬러 넣어주기
+document.querySelector('#cast').innerHTML=title,function(){var a=window.location.href,b=document.querySelectorAll('.gnb_2depth > li > a');b.forEach(function(b){-1!=a.indexOf(b.getAttribute('href'))&&b.classList.add('on')})}();
--- Seum/js/lib/include.js
+++ Seum/js/lib/include.js
@@ -1,18 +1,1 @@
-'use strict';
-
-window.addEventListener('load', function () {
-    var allElements = document.getElementsByTagName('*');
-    Array.prototype.forEach.call(allElements, function (el) {
-        var includePath = el.dataset.includePath;
-        if (includePath) {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    el.outerHTML = this.responseText;
-                }
-            };
-            xhttp.open('GET', includePath, true);
-            xhttp.send();
-        }
-    });
-});
+'use strict';window.addEventListener('load',function(){var a=document.getElementsByTagName('*');Array.prototype.forEach.call(a,function(a){var b=a.dataset.includePath;if(b){var c=new XMLHttpRequest;c.onreadystatechange=function(){4==this.readyState&&200==this.status&&(a.outerHTML=this.responseText)},c.open('GET',b,!0),c.send()}})});
